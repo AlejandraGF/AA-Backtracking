224de60eacd4b9ae6bc45fc09e0b22288d0b9103
David <dahercal29@gmail.com>
2021-03-21 18:55:01 -0600

    Cambios para realizar la lógica

--- AA_BackTracking/src/main.java
+++ AA_BackTracking/src/main.java
@@ -13,9 +13,12 @@ public class main {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola Backtracking");
+        int numero = (int) (Math.random() * 36);
+        System.out.println(numero);
+        
     }
     
 }
